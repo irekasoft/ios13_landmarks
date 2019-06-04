d184f4ac5b528a414983b404d8f8c5edeee587a9
Hijazi (MBP, Retina) <irekasoft@gmail.com>
2019-06-04 13:59:37 +0800

    first commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Hijazi on 4/6/19.
+//  Copyright © 2019 iReka Soft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+      
+      HStack(){
+        Text("Hello 00")
+        Text("Hello 00")
+        Text("Hello 00")
+      }
+     
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
